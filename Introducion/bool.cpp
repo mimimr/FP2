067c05930dd3551479000a5fac115175e5bd21b4
mimimr <nohemi.mendoza@my.unitec.edu.mx>
2024-10-14 11:32:05 -0600

    Create bool.cpp

---
+++ Introducion/bool.cpp
@@ -0,0 +1,23 @@
+// Clase 14 de Octubre 2024
+
+#include <iostream>
+using namespace std;
+bool;
+
+int main()
+{
+    // Operadores Logicos (true, false)
+    // and (disyunsign)
+    // or (conjuncion)
+    // not (not(true) => false)
+
+    cout << boolalpha; // imprime true en su lugar de 1
+    bool bb = true;
+    cout << bb << endl;
+    char 1 caracter : alpha, numerico, caracterespecial;
+    char opcion1 = 'a';
+    cout << opcion1 << endl;
+
+
+    std::cout << "Hello World!\n";
+}
