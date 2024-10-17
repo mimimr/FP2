a80ea6ad37b2c54edae003d6fcd76c7344a5f7a1
mimimr <nohemi.mendoza@my.unitec.edu.mx>
2024-10-17 12:41:15 -0600

    Create PRACTICA#B.cpp

---
+++ Introducion/PRACTICA#B.cpp
@@ -0,0 +1,30 @@
+// TAREA OPERACIONES
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 1.1 Operaciones sencillas
+    cout << "Resultado ejercicio 1: " << (3 + 5 * 2 / 2 - 1) << endl;
+    cout << "Resultado ejercicio 2: " << (3 + 10 / 2 - 1) << endl;
+    cout << "Resultado ejercicio 3: " << (3 + 5 - 1) << endl;
+    cout << "Resultado ejercicio 4: " << (8 - 1) << endl;
+    cout << "Resultado ejercicio 5: " << (1 / 3 * 3) << endl;
+
+    // 1.2. Resultado de una potencia
+    int num = 4;
+    cout << "La potencia de 4 es: " << (num * num) << endl;
+
+    // 1.3 Convertir a yardas
+    
+    //1.4 Problema 1
+    int valor = 200000;
+    cout << "Devaluacion es: " << (valor * 0.8) << endl;
+    cout << "Por Mes:" << ((valor * 0.8) / 5) << endl;
+
+    // 5. Porcentaje de deduccion
+
+
+    std::cout << "Hello World!\n";
+}
