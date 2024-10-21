c8758a089c0ae477396d638a1132b217f2e716c6
mimimr <nohemi.mendoza@my.unitec.edu.mx>
2024-10-21 11:21:44 -0600

    Create IF_ELSE ejercicio.cpp

---
+++ Introducion/IF_ELSE ejercicio.cpp
@@ -0,0 +1,21 @@
+//ejercicios de IF... ELSE
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2;
+    cout << "n1: ";
+    cin >> n1;
+    cout << endl;
+    if (n1 > n2)
+    {
+        cout << "n1: " << n1 << endl;
+
+    }
+    else 
+    {
+        cout << "n2: " << n2 << endl;
+
+    }
