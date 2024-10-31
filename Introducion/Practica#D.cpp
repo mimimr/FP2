12aa871b2909b2d9e298936f8829f44d8830303a
mimimr <nohemi.mendoza@my.unitec.edu.mx>
2024-10-31 11:56:56 -0600

    Create Practica#D.cpp

---
+++ Introducion/Practica#D.cpp
@@ -0,0 +1,39 @@
+// Practica #D
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 1.16 
+    int A, B, C;
+    cout << "Incerte un numero: ";
+    cin >> A;
+    cout << endl;
+
+    cout << "Incerte un numero: ";
+    cin >> B;
+    cout << endl;
+
+    cout << "Incerte numero: ";
+    cin >> C;
+    cout << endl;
+
+    if (A < B)
+    {
+        cout << "B es mayor" << endl;
+    }
+    if (A < C)
+    {
+        cout << "C es mayor" << endl;
+    }
+    if (A > B and A > C)
+    {
+        cout << "A es mayor" << endl;
+    }
+
+    // 1.17
+
+
+    std::cout << "Hello World!\n";
+}
