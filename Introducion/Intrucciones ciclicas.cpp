3e0d3dadd85899c7b76132672e053a2f942f0b27
mimimr <nohemi.mendoza@my.unitec.edu.mx>
2024-11-04 11:40:36 -0600

    Update Intrucciones ciclicas.cpp

--- Introducion/Intrucciones ciclicas.cpp
+++ Introducion/Intrucciones ciclicas.cpp
@@ -7,12 +7,26 @@ int main()
 {
     // Parte 1. While ():
     // Es una instrucción cíclica que valida un argumento booleano, mientras se cumpla este, seguirá ejecutando las instrucciones que estan dentro de los límites de ella
+    
+    // Ejercicio 1: 
     int i = 0;
     while (i <= 9)
     {
         cout << i << ",";
         i++;
     }
+    // Ejercicio 2:
+    int i = 0, suma = 0;
+    while (i <= 9)
+    {
+        cout << i << ",";
+        suma = suma + i;
+        i++;
+    }
+    cout << endl;
+    cout << "Total de la suma" << suma << endl;
+
+
 
     std::cout << "Hello World!\n";
 }
