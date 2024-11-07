9901c3007c7559cf485b5ede7f8e7f11630ba4ba
mimimr <nohemi.mendoza@my.unitec.edu.mx>
2024-11-07 12:05:48 -0600

    Create PRACTICA#E.cpp

---
+++ Introducion/PRACTICA#E.cpp
@@ -0,0 +1,48 @@
+/ Practica #E
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 1.21
+    
+    // 1.22
+
+    
+    // 1.23
+    
+
+    // 1.24 SRAND minijuego
+    srand(time(NULL)); // Semilla aleatoria
+    int castigo = 0;
+    int vida = 12;
+    int premio = 0;
+    cout << "vida inicial: " << vida << endl;
+    while (vida > 0)
+    {
+        //deducir una vida
+        vida--;
+        cout << "costo de ciclo:" << vida << endl;
+        //castigo?
+        castigo = rand() % 2;
+        if (castigo == 1)
+        {
+            vida = vida - 3;
+            cout << "Castigo" << vida << endl;
+        }
+        //Premio
+        premio = rand() % 2;
+        if (premio == 1)
+        {
+            vida = vida + 3;
+            cout << "Premio" << vida << endl;
+
+        }
+        cout << "Vida despues de ciclo:" << vida << endl;
+
+    }
+    
+
+    std::cout << "Hello World!\n";
+}
