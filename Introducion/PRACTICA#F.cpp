6f62167d73cf101799ff80647efb1c525135ef6d
mimimr <nohemi.mendoza@my.unitec.edu.mx>
2024-11-11 11:37:12 -0600

    Create PRACTICA#F.cpp

---
+++ Introducion/PRACTICA#F.cpp
@@ -0,0 +1,20 @@
+//PRACTICA #F
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Ejercicio 3. Numeros pares
+    int i = 2;
+    
+    if (i % 2 == 0)
+    {
+        cout << "Contar los numeros pares" << i << endl;
+    }
+    else
+    {
+        cout << "No numero par" << endl;
+    }
+
+    std::cout << "Hello World!\n";
+}
