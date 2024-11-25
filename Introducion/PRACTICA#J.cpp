0b155550493ef05b978e7ec92cef3fd4f89f4d41
mimimr <nohemi.mendoza@my.unitec.edu.mx>
2024-11-25 11:28:35 -0600

    Update PRACTICA#J.cpp

--- Introducion/PRACTICA#J.cpp
+++ Introducion/PRACTICA#J.cpp
@@ -17,6 +17,29 @@ int main()
         cout << elemento << endl;
     }
 
+
+    //  Ejercicio 1:
+    int numeros[10];
+    int num, i = 0;
+    num = -1;
+    while (num != 0)
+    {
+        cout << "Numero: ";
+        cin >> num;
+        if (num == 0 and i > 9)
+        {
+            break;
+        }
+        numeros[i] = num;
+        i++;
+    }
+    for (int elemento : numeros)
+    {
+        cout << elemento << endl;
+    }
+
+
+    
     // Ejercicio 4.
     srand(time(NULL));
     int total = 0;
