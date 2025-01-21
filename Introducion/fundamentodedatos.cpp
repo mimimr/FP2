45af2bd49bbb9fbc366351f9806d4a94bedcb30e
mimimr <nohemi.mendoza@my.unitec.edu.mx>
2025-01-21 12:50:15 -0600

    Create fundamentodedatos.cpp

---
+++ Introducion/fundamentodedatos.cpp
@@ -0,0 +1,13 @@
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int v, d, t;
+    cout << "Dame la distancia: " << endl;
+    cin >> d;
+    cout << endl;
+
+    std::cout << "Hello World!\n";
+}
